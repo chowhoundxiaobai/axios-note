e51fc3c9ff98fa7bd1a3fc04f712695b016e3514
chowhound <1069124473@qq.com>
2021-04-13 14:52:32 +0800

    init the project

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',//history 模式   默认是hash 模式，有#
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/store',
+      name: 'Store',
+      component: () => import('../views/Store.vue')//路由懒加载
+    },
+    {
+      path: '/other',
+      name: 'other',
+      component: () => import('../views/other.vue')//路由懒加载
+    },
+    {
+      path: '/axios',
+      name: 'axios',
+      component: () => import('../views/axios.vue')//路由懒加载
+    }
+  ]
+})
